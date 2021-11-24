aefbc3b9aea34200dcff756260535aa887768e80
Galih wisnuaji <code.yuune@gmail.com>
2021-11-24 09:39:39 +0700

    chore : update meta tag

--- mashu/src/pages/_app.tsx
+++ mashu/src/pages/_app.tsx
@@ -1,19 +1,36 @@
 import '../styles/global.css';
 import 'webrtc-adapter';
-
+import Head from 'next/head';
 import type { AppProps /*, AppContext */ } from 'next/app';
 import { AuthContextProvider } from '../modules/auth_provider';
 import { WebSocketProvider } from '../modules/websocket_provider';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <AuthContextProvider>
-      <WebSocketProvider>
-        <div className="flex flex-col h-full min-h-screen font-sans text-base antialiased bg-dark-primary text-white transition-all duration-300 md:flex-row">
-          <Component {...pageProps} />
-        </div>
-      </WebSocketProvider>
-    </AuthContextProvider>
+    <>
+      <Head>
+        <title>MoeChat</title>
+        <meta name="application-name" content="moechat" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta
+          name="msapplication-config"
+          content="/static/icons/browserconfig.xml"
+        />
+        <meta name="msapplication-TileColor" content="#1b2224" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#1b2224" />
+      </Head>
+      <AuthContextProvider>
+        <WebSocketProvider>
+          <div className="flex flex-col h-full min-h-screen font-sans text-base antialiased bg-dark-primary text-white transition-all duration-300 md:flex-row">
+            <Component {...pageProps} />
+          </div>
+        </WebSocketProvider>
+      </AuthContextProvider>
+    </>
   );
 }
 
